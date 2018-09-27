40f88ebf649d958eefb575c70ecff8b252004d7f
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-09-27 19:33:46 -0300

    Add uniqueness validation spec

--- spec/models/client_spec.rb
+++ spec/models/client_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe Client, type: :model do
   it { should validate_presence_of(:name) }
   it { should validate_presence_of(:email) }
+  it { should validate_uniqueness_of(:name) }
 end
