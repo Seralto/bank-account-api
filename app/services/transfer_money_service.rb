70b3f26db30f8e9845faaaba54d81b6d5f1cc90c
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-09-28 18:51:23 -0300

    Fix TransferMoneyService

--- app/services/transfer_money_service.rb
+++ app/services/transfer_money_service.rb
@@ -1,14 +1,14 @@
 class TransferMoneyService
-  attr_accessor :source_account_id, :destination_account_id, :amount
+  attr_accessor :client_id, :destination_account_id, :amount
 
   def initialize(params)
-    @source_account_id = params['id']
+    @client_id = params['id']
     @destination_account_id = params['destination_account_id']
     @amount = params['amount'].to_f
   end
 
   def perform
-    source_account = Account.find(source_account_id)
+    source_account = Client.find(client_id).account
     return { success: false, message: 'Not enough money.' } if source_account.balance < amount
 
     destination_account = Account.find(destination_account_id)
