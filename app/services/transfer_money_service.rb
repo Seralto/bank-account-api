e83a81e7ab02383fb1895b56fc7f6abc76a5c384
Sérgio Toledo <contato@sergiotoledo.com.br>
2018-09-28 00:19:10 -0300

    TransferMoneyService refactor

--- app/services/transfer_money_service.rb
+++ app/services/transfer_money_service.rb
@@ -4,22 +4,22 @@ class TransferMoneyService
   def initialize(params)
     @source_account_id = params['id']
     @destination_account_id = params['destination_account_id']
-    @amount = params['amount']
+    @amount = params['amount'].to_f
   end
 
   def perform
     source_account = Account.find(source_account_id)
-    return { success: false, message: 'Not enougth money.' } if source_account.balance.to_f < amount.to_f
+    return { success: false, message: 'Not enougth money.' } if source_account.balance < amount
 
     destination_account = Account.find(destination_account_id)
 
     Client.transaction do
-      source_account.update!(balance: source_account.balance.to_f - amount.to_f)
-      destination_account.update!(balance: destination_account.balance.to_f + amount.to_f)
+      source_account.update!(balance: source_account.balance - amount)
+      destination_account.update!(balance: destination_account.balance + amount)
 
       { success: true, message: 'Transfer completed.' }
     end
-  rescue => e
+  rescue StandardError => e
     { success: false, message: e }
   end
 end
